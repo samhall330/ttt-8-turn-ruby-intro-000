b4e448616e972b59cb9efde04a1ed6d0fc24ea7f
Sam Hall <mann.samantha11@gmail.com>
2020-10-22 17:37:09 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,6 +38,11 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   valid_move?(board, index)
-
-  move(board, index, "X")
+  while valid_move (board, index) == false
+    puts "Please enter 1-9:"
+  end
+    
+    
+  end
+  
 end
