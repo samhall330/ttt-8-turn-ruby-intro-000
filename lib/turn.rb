28ab09ce1e2174e0a4537ca255986151fc3f38fe
Sam Hall <mann.samantha11@gmail.com>
2020-10-22 17:54:00 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -39,6 +39,6 @@ def turn(board)
   index = input_to_index(user_input)
     if valid_move?(board, index) == true
     move(board, index, "X")
-    turn(board)
+    else turn(board)
     display_board(board)
 end
