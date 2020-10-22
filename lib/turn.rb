060d58a16dd6a571457e598ccb882bd126ccbc16
Sam Hall <mann.samantha11@gmail.com>
2020-10-22 16:40:07 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,3 +16,19 @@ def move(array, index, value = "X")
   array[index] = value
   return board
 end
+
+def valid_move?(board, index)
+  if position_taken?(board, index) == true
+    false
+  elsif index.between?(0,8)
+    true
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else board[index] == "X" || board[index] == "O"
+    true
+  end
+end
